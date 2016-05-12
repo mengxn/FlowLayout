fcdefbcdb601d85cac014221263f765b4edafce1
captainJeck <35630903@qq.com>
2016-05-12 23:33:03 +0800

    init FlowLayout

---
+++ app/src/main/java/me/codego/flowlayout/FlowLayout.java
@@ -0,0 +1,51 @@
+package me.codego.flowlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * 流式布局FlowLayout
+ * Created by mengxn on 16/5/10.
+ */
+public class FlowLayout extends FrameLayout {
+
+    public FlowLayout(Context context) {
+        super(context);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int parentWidth = getMeasuredWidth();
+
+        int maxWidth = 0;
+        int maxHeight = 0;
+        int lineHeight = 0;
+
+        for (int i = 0; i < getChildCount(); i++) {
+            View childView = getChildAt(i);
+            childView.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED), MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+            LayoutParams params = (LayoutParams) childView.getLayoutParams();
+            if (maxWidth + childWidth > parentWidth) {
+                maxHeight += lineHeight;
+                params.topMargin = maxHeight;
+                params.leftMargin = 0;
+                maxWidth = childWidth;
+            } else {
+                params.leftMargin = maxWidth;
+                params.topMargin = maxHeight;
+                maxWidth += childWidth;
+            }
+            lineHeight = Math.max(lineHeight, childHeight);
+        }
+    }
+}
---
+++ app/src/main/java/me/codego/flowlayout/MainActivity.java
@@ -0,0 +1,37 @@
+package me.codego.flowlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FlowLayout flowLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        flowLayout = (FlowLayout) findViewById(R.id.flow_layout);
+
+        flowLayout.addView(initTag("hello world !"));
+        flowLayout.addView(initTag("welcome to my world !"));
+        flowLayout.addView(initTag("Hi, I'm Rock"));
+        flowLayout.addView(initTag("Let's do it !"));
+        flowLayout.addView(initTag("beautiful girl"));
+        flowLayout.addView(initTag("shot box"));
+        flowLayout.addView(initTag("android developer"));
+        flowLayout.addView(initTag("stay foolish, stay hungry!"));
+        flowLayout.addView(initTag("Mac book"));
+        flowLayout.addView(initTag("iPhone"));
+    }
+
+    private View initTag(String tag) {
+        View view = getLayoutInflater().inflate(R.layout.item_tag, null);
+        TextView tagView = (TextView) view.findViewById(R.id.tag);
+        tagView.setText(tag);
+        return view;
+    }
+}
